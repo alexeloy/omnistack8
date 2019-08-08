ad74612b8b6ed42d8cfe7c5832160ed4ce3e0150
Alex Eloy <alexeloycg@gmail.com>
2019-08-08 16:19:40 -0300

    código Mobile React Native

--- frontend/src/pages/main.js
+++ frontend/src/pages/main.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './main.css';
 
 import api from '../services/api';
@@ -24,32 +25,53 @@ export default function Main({ match }) {
     loadUsers();
   }, [match.params.id]);
 
+  async function handleLike(id) {
+    console.log('like', id)
+  }
+
+  async function handleDislike(id) {
+    console.log('dislike', id);
+    await api.post(`/devs/${id}/dislikes`, null, {
+      headers: { user: match.params.id },
+    })
+
+    setUsers(users.filter(user => user._id != id));
+  }
   return (
     <div className="main-container">
-      <img src={logo} alt="Tindev" ></img>
-      <ul>
-        {users.map(user => (
+      <Link to="/">
+        <img src={logo} alt="Tindev" ></img>
+      </Link>
+      {users.length > 0 ? (
+        <ul>
+          {users.map(user => (
 
-          <li key={user._id}>
-            <img src={user.avatar} alt={user.name}></img>
-            <footer>
-              <strong>{user.name}</strong>
-              <p>{user.bio}</p>
-            </footer>
-            <div className="buttons">
-              <button type="button">
-                <img src={like} alt="Like"></img>
-              </button>
+            <li key={user._id}>
+              <img src={user.avatar} alt={user.name}></img>
+              <footer>
+                <strong>{user.name}</strong>
+                <p>{user.bio}</p>
+              </footer>
+              <div className="buttons">
+                <button type="button" onClick={() => handleDislike(user._id)}>
+                  <img src={dislike} alt="Dislike"></img>
+                </button>
 
-              <button type="button">
-                <img src={dislike} alt="Dislike"></img>
-              </button>
-            </div>
-          </li>
-        ))}
+                <button type="button" onClick={() => handleLike(user._id)} >
+                  <img src={like} alt="Like"></img>
+                </button>
+              </div>
+            </li>
+          ))
+          }
+        </ul>
+
+      ) : (
+          <div className="empty">Acabou :(</div>
+        )}
 
 
-      </ul>
+
     </div>
   )
 }
---
+++ tindev/src/pages/Main.js
@@ -0,0 +1,177 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import logo from '../assets/logo.png';
+import dislike from '../assets/dislike.png';
+import like from '../assets/like.png';
+import { SafeAreaView } from 'react-navigation';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import AsyncStorage from '@react-native-community/async-storage'
+
+import api from '../services/api';
+
+export default function Main({ navigation }) {
+  const id = navigation.getParam('user');
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await api.get('/devs', {
+        headers: {
+          user: id,
+        }
+      })
+
+      setUsers(response.data);
+    }
+    loadUsers();
+  }, [id]);
+
+  async function handleLike() {
+    //mesma coisa que
+    //const user = users[0]; // pega só o primeiro elemento
+    const [user, ...rest] = users;
+
+    await api.post(`/devs/${user._id}/dislikes`, null, {
+      headers: { user: id },
+    })
+    setUsers(rest);
+  }
+
+  async function handleDislike() {
+
+    const [user, ...rest] = users;
+    await api.post(`/devs/${user._id}/dislikes`, null, {
+      headers: { user: id },
+    })
+
+    setUsers(rest);
+  }
+
+  async function handleLogout() {
+    await AsyncStorage.clear();
+    navigation.navigate('Login');
+  }
+  return (
+    <SafeAreaView style={styles.container}  >
+      <TouchableOpacity onPress={handleLogout}>
+        <Image source={logo} style={styles.logo} ></Image>
+      </TouchableOpacity>
+
+
+      <View style={styles.cardsContainer}>
+        {users.length === 0 ?
+          <Text style={styles.empty}>Acabou :(</Text>
+          :
+          (
+
+            users.map((user, index) => (
+              <View key={user._id} style={[styles.card, { zIndex: users.length - index }]}>
+                <Image style={styles.avatar} source={{ uri: user.avatar }}></Image>
+                <View style={styles.footer}>
+                  <Text style={styles.name}>{user.name}</Text>
+                  <Text style={styles.bio} numberOfLines={3} >{user.bio}</Text>
+                </View>
+              </View>
+            ))
+          )}
+
+      </View>
+
+
+
+
+      {users.length > 0 &&
+        (
+          <View style={styles.buttonsContainer}>
+            <TouchableOpacity style={styles.button} onPress={handleDislike}>
+              <Image source={dislike}></Image>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={handleLike}>
+              <Image source={like}></Image>
+            </TouchableOpacity>
+          </View>
+        )}
+
+
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  logo: {
+    marginTop: 30
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  cardsContainer: {
+    flex: 1,
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    maxHeight: 500,
+
+  },
+  card: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 8,
+    margin: 30,
+    overflow: 'hidden',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
+  },
+  avatar: {
+    flex: 1,
+    height: 300
+  },
+  footer: {
+    backgroundColor: '#fff',
+    paddingHorizontal: 20,
+    paddingVertical: 15
+  },
+  name: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#333'
+  },
+  bio: {
+    fontSize: 14,
+    color: '#999',
+    marginTop: 5,
+    lineHeight: 18
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    marginBottom: 30
+  },
+  button: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 20,
+    elevation: 2,
+    //Para o ios
+    shadowColor: '#000',
+    shadowOpacity: 0.05,
+    shadowRadius: 2,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    }
+  },
+  empty: {
+    alignSelf: 'center',
+    color: '#999',
+    fontSize: 24,
+    fontWeight: 'bold'
+  }
+})
